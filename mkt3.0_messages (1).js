40716ea49f3f0a80413ea980c5dddf1ea384a1a5
neelamratnam10-blip <neelamratnam10@gmail.com>
2026-08-11 17:53:45 +0530

    Update mkt3.0_messages (1).js

--- mkt3.0_messages (1).js
+++ mkt3.0_messages (1).js
@@ -1,5 +1,5 @@
 Mkt3L10n.adminFieldHtmlEncodeForm = {
-  Html_Encode_Tokens: "HTML Encode Tokens in EmaiZ dwsdawsfaws tokrn",
+  Html_Encode_Tokens: "HTML Encode Tokens in EmaiZ dwsdawsfaws  check check check check check",
   Explain:
     "Configuressss the default HTML encoding behavior in email tokenss for all fieldssss gghfjhvjhvhjvhjvhjvhj",
   Remind:
